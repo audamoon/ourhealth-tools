b8b25665bc28258b17c81ef270851cb2b82afce2
Nikita <audamoon@gmail.com>
2023-08-04 11:59:20 +0300

    :cd: upd: change_domen

--- main/yandex/change_domen.py
+++ main/yandex/change_domen.py
@@ -5,26 +5,30 @@ from time import sleep
 gs = SheetManager("1WsAf_t2PuLD4qowmv5zYyp7YMNpyuSsN0Ir35u5aWQk")
 sm = SeleniumManager()
 
-a = gs.get_values("C")
-row_array = []
-for j in range(len(a)):
-    for el in a[j]:
-        if el == "Что-то не так":
-            row_array.append(j)
-for row_id in row_array:
-    old_url = gs.read_cell("A",True,row_id)
-    new_url = gs.read_cell("B",True,row_id)
+# a = gs.get_values("C")
+# row_array = []
+# for j in range(len(a)):
+#     for el in a[j]:
+#         if el == "Что-то не так":
+#             row_array.append(j+1)
+# for row_id in row_array:
+for i in range (2,453):
+    old_url = gs.read_cell("A",True,i)
+    new_url = gs.read_cell("B",True,i)
     chopped_old_url = old_url.split("//")
     chopped_new_url = new_url.split("//")
     link = f"https://webmaster.yandex.ru/site/{chopped_old_url[0]}{chopped_old_url[1]}:443/indexing/mirrors/"
     sm.driver.get(link)
     sleep(2)
-try: 
-    sm.driver.find_element(By.XPATH,"//span[contains(text(),'В ближайшее время')]")
-    sm.driver.find_element(By.XPATH,"//span[contains(text(),'Заявка от')]")
-    gs.write_cell("C","Добавлен",True,row_id)
-except:
-    gs.write_cell("C","Что-то не так",True,row_id)
+    try: 
+        sm.driver.find_element(By.XPATH,"//span[contains(text(),'Заявка от')]")
+        gs.write_cell("C","Добавлен",True,i)
+    except:
+        try:
+            sm.driver.find_element(By.XPATH,"//span[contains(text(),'В ближайшее время')]")
+            gs.write_cell("C","Добавлен",True,i)
+        except:
+            gs.write_cell("C","Что-то не так",True,i)
 
 
     # добавление домена
